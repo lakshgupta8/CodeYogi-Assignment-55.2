c81c3347c9b7980aebe31f93ff01faf28193ec29
Lakshya Gupta <7351086263kshama@gmail.com>
2025-10-22 12:09:57 +0530

    adjusted arrow functions

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Footer = () => {
+function Footer () {
   return (
     <footer className="bg-gray-700 text-white flex py-4 justify-between flex-wrap px-10 sm:px-20 lg:px-40">
       <p className="text-xs">Copyright © 2022 | CodeYogi</p>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Navbar = () => {
+function Navbar () {
   return (
     <div className="px-10 sm:px-20 lg:px-40 flex bg-white">
       <nav className="w-1/2 self-center bg-white">
